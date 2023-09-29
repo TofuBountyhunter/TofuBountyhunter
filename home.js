542f11d407fde32cabf3ac42676ba586fade4ac7
TofuBountyhunter <141579510+TofuBountyhunter@users.noreply.github.com>
2023-09-29 16:17:29 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+function fun(){
+    const action1= document.querySelector(".action1");
+    const action2= document.querySelector(".action2");
+
+    action1.style.display="none"
+    action2.style.display="block"
+
+}
